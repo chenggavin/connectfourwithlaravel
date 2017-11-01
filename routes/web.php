8c1cafcc5f6d6c95af540bad08285d765a644566
Gavin Cheng <gavincheng01@gmail.com>
2017-11-01 11:15:19 -0400

    Added restart button

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,7 +9,61 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+  // TODO: fix this mess, it's broken
+  $players = ['Red', 'Blue'];
+  $rows = 6;
+  $columns = 7;
+  $turn = 5;
+  $currentPlayer = $players[$turn % 2];
+  $board = [];
+  for ($r = 0; $r < $rows; $r++) {
+    for ($c = 0; $c < $columns; $c++) {
+      $board[$r][$c] = '';
+    }
+  }
+  return view('board', compact('currentPlayer', 'turn', 'board', 'rows', 'columns'));
+})->name('board');
+Route::get('game/{id}', function($id) {
+  $game = \App\Game::find($id);
+  $turn = $game->turn;
+  $rows = $game->rows;
+  $columns = $game->columns;
+  $currentPlayer = $game->players[$turn % 2];
+  $board = [];
+  for ($r = 0; $r < $rows; $r++) {
+    for ($c = 0; $c < $columns; $c++) {
+      $board[$r][$c] = '';
+    }
+  }
+  return view('board', compact('currentPlayer', 'turn', 'board', 'rows', 'columns'));
+})->name('game');
+Route::get('/restart', function() {
+
+  // TODO: End the old game
+  // ??? set in_progress to false ???
+// ^ ^ ^ Not safe until user logins are added^  ^ ^
+
+
+
+
+  // Make a new game
+  $game = new \App\Game;
+  $game->save();
+  $game->turn = 1;
+  $id = $game->id;
+  // Show the board
+  return redirect()->route('game', ['id' => $id]);
 });
+
+
+
+
+
+
+
+
+
+
+
+
